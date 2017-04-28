e78396b6322f0f2aabe5e8e2b6f31c6570f6d569
Matt Vukas <mattvukas@gmail.com>
2017-04-27 19:53:54 -0600

    remove unneeded template endpoints, add coding challenge endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const request = require('request');
+const express = require('express');
+const cheerio = require('cheerio');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router();
+
+// coding challenge
+router.get('/api/products/:id', function(req, res, next) {
+  const baseUrl = "https://looplist-product-sample.herokuapp.com/products/";
+  request(baseUrl + req.params.id, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const $ = cheerio.load(body);
+      res.send({
+        title: $("title").text(),
+        name: $("h2.product-name").text(),
+        image: $("div.product-image-group img").attr("src"),
+        description: $("div.card:contains('Description:')").find("p").text() // could also select 4th card
+      })
+    } else {
+      res.status(503).send({ // service unavailable
+        message: "Sorry, unable to fetch product page"
+      })
+    }
+  })
 });
 
 module.exports = router;
